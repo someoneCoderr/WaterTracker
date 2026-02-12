6dba296454ca35ae8ed0dc63323665280c088fb4
Silas Haelg <silas.haelg@bluewin.ch>
2026-02-12 20:54:26 +0100

    Add project files.

---
+++ WaterTracker/ViewModel/HomeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using AppRunner.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRunner.ViewModel
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        private int _drankWater;
+
+        public HomeViewModel()
+        {
+
+        }
+
+        public int DrankWater
+        {
+            get { return _drankWater; }
+            set
+            {
+                _drankWater = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddWaterCommand { get; }
+
+        //public void AddWater()
+        //{
+        //    DrankWater += 
+        //}
+    }
+}
---
+++ WaterTracker/Views/HomeView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppRunner.ViewModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppRunner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            HomeViewModel vm = new HomeViewModel();
+            this.DataContext = vm;
+            InitializeComponent();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // Hide instead of closing
+            e.Cancel = true;
+            Hide();
+        }
+
+    }
+}
